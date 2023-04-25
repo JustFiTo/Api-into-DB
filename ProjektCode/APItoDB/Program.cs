e154c30ff15e0e262f95460208343a71ef323d48
finn-torben.clas <finn-torben.clas@mailbox.org>
2023-04-25 12:22:29 +0200

    C# projekt erstellt

---
+++ ProjektCode/APItoDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APItoDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
