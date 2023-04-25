dd1a1fe220c142bff695981fae8e234af715228f
finn-torben.clas <finn-torben.clas@mailbox.org>
2023-04-25 12:42:11 +0200

    Start der klassen

---
+++ ProjektCode/APItoDB/ApiAnbindung.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APItoDB
+{
+    class ApiAnbindung
+    {
+        private String url;
+        private String requestType;
+        private String input;
+
+        public ApiAnbindung(String url, String requestType, String input)
+        {
+            this.url = url;
+            this.requestType = requestType;
+            this.input = input;
+        }
+    }
+}
--- ProjektCode/APItoDB/Program.cs
+++ ProjektCode/APItoDB/Program.cs
@@ -7,6 +7,7 @@ namespace APItoDB
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
