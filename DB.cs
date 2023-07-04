cb6586ec5b2b75b0e58f3c5a272a715ce2de9346
AnnanasM <anna.moutidis@its-stuttgart.de>
2023-07-04 13:57:33 +0200

    feat(windDeg): add letters for wind direction

--- DB.cs
+++ DB.cs
@@ -17,12 +17,46 @@ namespace APItoDB
             string dateString = date.AddSeconds(weathermap.dt).ToString("dd.MM.yyyy HH:mm:ss");
             string sunsetString = date.AddSeconds(weathermap.sys.sunset).ToString("HH:mm:ss");
             string sunriseString = date.AddSeconds(weathermap.sys.sunrise).ToString("HH:mm:ss");
+            string deg = "";
+
+            if (weathermap.wind.deg > 0 && weathermap.wind.deg < 45)
+            {
+                deg = "N";
+            }
+            else if (weathermap.wind.deg > 45 && weathermap.wind.deg < 90)
+            {
+                deg = "NO";
+            }
+            else if (weathermap.wind.deg > 90 && weathermap.wind.deg < 135)
+            {
+                deg = "O";
+            }
+            else if (weathermap.wind.deg > 135 && weathermap.wind.deg < 180)
+            {
+                deg = "SO";
+            }
+            else if (weathermap.wind.deg > 180 && weathermap.wind.deg < 225)
+            {
+                deg = "S";
+            }
+            else if (weathermap.wind.deg > 225 && weathermap.wind.deg < 270)
+            {
+                deg = "SW";
+            }
+            else if (weathermap.wind.deg > 270 && weathermap.wind.deg < 315)
+            {
+                deg = "W";
+            }
+            else if (weathermap.wind.deg > 315 && weathermap.wind.deg < 360)
+            {
+                deg = "NW";
+            }
 
             MySqlConnection conn = connectToDb();
             conn.Open();
             var cmd = new MySqlCommand();
             cmd.Connection = conn;
-            cmd.CommandText = "INSERT INTO Weatherdata (dates, name, country, temp, feelslike, decription, visibility, windSpeed, windDeg, sunset, sunrise) VALUES ('" + dateString + "', '" + weathermap.name + "', '" + weathermap.sys.country + "'," + weathermap.main.temp + ", " +weathermap.main.feels_like + ", '" + weathermap.weather[0].description + "', " + weathermap.visibility + ", " + weathermap.wind.speed + ", " + weathermap.wind.deg + ", '" + sunsetString + "', '" + sunriseString + "');";
+            cmd.CommandText = "INSERT INTO Weatherdata (dates, name, country, temp, feelslike, decription, visibility, windSpeed, windDeg, sunset, sunrise) VALUES ('" + dateString + "', '" + weathermap.name + "', '" + weathermap.sys.country + "'," + weathermap.main.temp + ", " +weathermap.main.feels_like + ", '" + weathermap.weather[0].description + "', " + weathermap.visibility + ", " + weathermap.wind.speed + ", '" + deg + "', '" + sunsetString + "', '" + sunriseString + "');";
             cmd.ExecuteNonQuery();
             conn.Close();
         }
