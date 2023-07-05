b17b0e98a60a0c877793e74971265120494fc943
Nicholas Neumann <nichoneumann01@gmail.com>
2023-07-05 23:29:55 +0200

    UI mit API verknüpfen

--- Wpf UI/Wpf UI/MainWindow.xaml.cs
+++ Wpf UI/Wpf UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using APItoDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using System.Diagnostics.Metrics;
+using System.Net.Http;
+using System.Text.Json;
+using MySql.Data.MySqlClient;
 
 namespace WPF_UI
 {
@@ -23,6 +28,7 @@ namespace WPF_UI
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -32,37 +38,12 @@ namespace WPF_UI
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            string city = txtBox_Stadt.Text;
 
+            WeatherMap weatherMap = API.ResponseCurrent(city);
+
+            //DateTime date = txtBox_Von.Text();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
-        {
-            
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Gefühlt_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void TextLand_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
--- Wpf UI/Wpf UI/WeatherMap.cs
+++ Wpf UI/Wpf UI/WeatherMap.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections.Generic;
+using static Google.Protobuf.WellKnownTypes.Field.Types;
 
 namespace APItoDB
 {
     public class WeatherMap
     {
         public string name { get; set; }
+        public int timezone { get; set; }
+        public int dt { get; set; }
         public Sys sys { get; set; }
         public List<Weather> weather { get; set; }
         public Main main { get; set; }
-        public City city { get; set; }
         public List<Elements> date { get; set; }
+        public Wind wind { get; set; }
+        public float visibility { get; set; }
 
     }
 
