bb119b624f3245d1ca69992179e60655882f3144
AnnanasM <anna.moutidis@its-stuttgart.de>
2023-07-09 21:43:40 +0200

    refactor: chance folder structure + add sql db

---
+++ Wpf UI/Wpf UI/API.sql
@@ -0,0 +1,17 @@
+﻿CREATE DATABASE IF NOT EXISTS API;
+USE API;
+
+CREATE TABLE IF NOT EXISTS Weatherdata (
+    dates VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),  
+    country VARCHAR(255),
+    temp INT,
+    feelslike INT,
+    decription VARCHAR(255),
+    visibility INT,
+    windSpeed INT,
+    windDeg INT,
+    sunset VARCHAR(255),
+    sunrise VARCHAR(255)
+);
+
